f536520a22f789285ea22e98a4ad3ce2496e36c6
assisken <assisken@gmail.com>
2019-06-26 19:16:53 +0300

    Moved several tests to one test.

--- main/tests/unit_tests/schedule.py
+++ main/tests/unit_tests/schedule.py
@@ -8,48 +8,23 @@ from utils.date import TeachTime
 
 
 class ScheduleTest(TestCase):
-    def test_autumn_start(self):
+    def test_date_block(self):
         res = date_block(TeachTime(date(2012, 9, 3)))
-        expected = {
-            'text': 'Учёба продолжается',
-            'num': 2,
-            'desc': 'неделя'
-        }
+        expected = {'text': 'Учёба продолжается', 'num': 2, 'desc': 'неделя'}
         self.assertDictEqual(expected, res, msg='Dicts not equal. ')
 
-    def test_autumn_end(self):
         res = date_block(TeachTime(date(2016, 12, 31)))
-        expected = {
-            'text': 'Начало учёбы',
-            'num': 9,
-            'desc': 'февраля'
-        }
+        expected = {'text': 'Начало учёбы', 'num': 9, 'desc': 'февраля'}
         self.assertDictEqual(expected, res, msg='Dicts not equal. ')
 
-    def test_autumn_end2(self):
         res = date_block(TeachTime(date(2016, 1, 1)))
-        expected = {
-            'text': 'Начало учёбы',
-            'num': 9,
-            'desc': 'февраля'
-        }
+        expected = {'text': 'Начало учёбы', 'num': 9, 'desc': 'февраля'}
         self.assertDictEqual(expected, res, msg='Dicts not equal. ')
 
-    @freeze_time('2019-02-14')
-    def test_spring_start(self):
-        res = date_block(TeachTime(date.today()))
-        expected = {
-            'text': 'Учёба продолжается',
-            'num': 2,
-            'desc': 'неделя'
-        }
+        res = date_block(TeachTime(date(2019, 2, 14)))
+        expected = {'text': 'Учёба продолжается', 'num': 2, 'desc': 'неделя'}
         self.assertDictEqual(expected, res, msg='Dicts not equal. ')
 
-    def test_spring_end(self):
         res = date_block(TeachTime(date(2027, 7, 1)))
-        expected = {
-            'text': 'Начало учёбы',
-            'num': 1,
-            'desc': 'сентября'
-        }
+        expected = {'text': 'Начало учёбы', 'num': 1, 'desc': 'сентября'}
         self.assertDictEqual(expected, res, msg='Dicts not equal. ')
