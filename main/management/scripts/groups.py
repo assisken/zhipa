70ef5112aa6b608710031673a8151fc86f7c8035
assisken <assisken@gmail.com>
2019-07-27 11:57:59 +0300

    Added check for lms response.

--- main/management/scripts/groups.py
+++ main/management/scripts/groups.py
@@ -5,8 +5,16 @@ import requests
 from main.models import Group
 
 
-def fetch_groups(url: str, password: str, department: str) -> None:
-    resp = requests.get(url.format(password=password, department=department))
+def fetch_groups(url_pattern: str, password: str, department: str) -> None:
+    url = url_pattern.format(password=password, department=department)
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        print('Seems like lms does not response with 200 code.')
+        print('Please, update website url or check that website does respond on url:')
+        print(url)
+        exit(1)
+
     data = json.loads(resp.content.decode('utf8'))['data']
 
     for group, value in data.items():
