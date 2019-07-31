abbc066890d6bc35371bdf725d94b3a010ed36d6
assisken <assisken@gmail.com>
2019-07-31 20:43:43 +0300

    Fixed lms loading. It responds with 500 code. D:

--- main/management/scripts/groups.py
+++ main/management/scripts/groups.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+from django.db import IntegrityError
 
 from main.models import Group
 
@@ -9,7 +10,7 @@ def fetch_groups(url_pattern: str, password: str, department: str) -> None:
     url = url_pattern.format(password=password, department=department)
     resp = requests.get(url)
 
-    if resp.status_code != 200:
+    if resp.status_code not in (200, 500):
         print('Seems like lms does not response with 200 code.')
         print('Please, update website url or check that website does respond on url:')
         print(url)
@@ -18,8 +19,11 @@ def fetch_groups(url_pattern: str, password: str, department: str) -> None:
     data = json.loads(resp.content.decode('utf8'))['data']
 
     for group, value in data.items():
-        Group.objects.create(
-            name=group,
-            semester=int(value['semester']),
-            study_form=value['study_form']
-        )
+        try:
+            Group.objects.create(
+                name=group,
+                semester=int(value['semester']),
+                study_form=value['study_form']
+            )
+        except IntegrityError:
+            print('Group {} is already exist! Skipping...'.format(group))
