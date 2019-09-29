ff154085bfe29e5450fa0a877ebc91ca78547a81
assisken <assisken@gmail.com>
2019-09-29 03:23:52 +0300

    Moved forms to separate file.

--- main/views/admin/couple_publications.py
+++ main/views/admin/couple_publications.py
@@ -1,34 +1,11 @@
-from django import forms
 from django.contrib import messages
 from django.contrib.messages import add_message
-from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from main.forms import SeveralPublicationsForm
 from main.models import Publication
-
-
-def check_items(value: str):
-    items = value.split('\n')
-    for line, item in enumerate(items):
-        if ' 	' or item:
-            if item.count(' 	') != 2:
-                count = item.count(' 	') + 1
-                raise ValidationError(f'(dad) Required 3 or more items, got {count} on line {line + 1}')
-            return
-        elif item.count('||') != 2:
-            count = item.count("||") + 1
-            raise ValidationError(f'{" 	" in item and item.count(" 	") != 2} Required 3 or more items, got {count} on line {line + 1}')
-
-
-class SeveralPublicationsForm(forms.Form):
-    error_css_class = 'errors'
-    required_css_class = 'required'
-    couple_items = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'vLargeTextField'}),
-        validators=(check_items,)
-    )
 
 
 class SeveralPublicationsView(TemplateView):
