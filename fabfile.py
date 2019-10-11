098bf32c61f2156c8e8c5708a319988143689e6f
assisken <assisken@gmail.com>
2019-10-11 22:21:14 +0300

    Added fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,31 @@
 import os
 
 from fabric import Connection, task, Config
-# from invoke.config import Config
 
 host = os.getenv('DEPLOY_HOST')
 port = os.getenv('DEPLOY_PORT')
 user = os.getenv('DEPLOY_USER')
 password = os.getenv('DEPLOY_PASSWORD')
 project_dir = os.getenv('PROJECT_DIR')
+service = os.getenv('SERVICE_NAME')
+python = os.getenv('PYTHON')
 
 config = Config({
-    'sudo': {
-        'password': password,
-        'prompt': '[sudo] '
-    },
-    'reject-unknown-hosts': True
+    'reject-unknown-hosts': True,
+    'shell': '/bin/bash -lic'
 })
 
 
 @task
 def deploy(ctx):
     with Connection(host=host, port=int(port), user=user,
-                    connect_kwargs={'password': password}, config=config) as con:
+                    connect_kwargs={'key_filename': 'deploy_key'}, config=config) as con:
         with con.cd(os.path.join('$HOME', project_dir)):
             con.run('git checkout master')
-            con.run('git pull')
+            con.run('git pull origin master')
+            with con.prefix(f'source {os.path.join("$HOME", project_dir, ".env", "bin", "activate")}'):
+                con.run(f'{python} manage.py migrate --noinput')
+                con.run(f'{python} manage.py collectstatic --noinput')
+            con.sudo(f'systemctl stop {service}')
+            con.sudo(f'systemctl start {service}')
+
