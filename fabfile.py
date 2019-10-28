28c8fc4877e8d6878a2ee699a7556f85e475bf2c
Nikita Zhiga <nvzhiga@avito.ru>
2019-10-29 01:01:12 +0300

    Trying to fix sudo.

--- fabfile.py
+++ fabfile.py
@@ -11,21 +11,23 @@ service = os.getenv('SERVICE_NAME')
 python = os.getenv('PYTHON')
 ci = os.getenv('CI')
 
-config = Config({
-    'sudo': {
-        'password': password,
-        'prompt': '[sudo] '
-    },
-    'reject-unknown-hosts': True,
-    'shell': '/bin/bash -lic'
-})
-
 
 @task
 def deploy(ctx):
     if not ci:
         print('Run is allowed only in CI!')
         exit(1)
+
+    config = Config({
+        'sudo': {
+            'password': password,
+            'prompt': '[sudo] '
+        },
+        'reject-unknown-hosts': True,
+        'shell': '/bin/bash -lic'
+    })
+
+    print(config)
     with Connection(host=host, port=int(port), user=user,
                     connect_kwargs={'password': password}, config=config) as con:
         with con.cd(os.path.join('$HOME', project_dir)):
@@ -36,5 +38,6 @@ def deploy(ctx):
                 con.run('pip3.7 install -r requirements.txt')
                 con.run(f'{python} manage.py migrate --noinput')
                 con.run(f'{python} manage.py collectstatic --noinput')
+        print(config)
         con.sudo(f'systemctl stop {service}')
         con.sudo(f'systemctl start {service}')
