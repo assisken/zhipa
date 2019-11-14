55e8825d47e45596c042787af588522f153af9d8
Nikita Zhiga <assisken@gmail.com>
2019-11-14 16:11:13 +0000

    Added teachers back. But now it's hidden.

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -1,7 +1,7 @@
 from .history_view import HistoryView
 from .staff_view import StaffView
 from .news_view import NewsListView, NewsDetailView, NewsDateListView, NewsDateDetailView
-from .timetable_view import GroupTimetableView, ExtramuralTimetableView
+from .timetable_view import GroupTimetableView, ExtramuralTimetableView, TeacherTimetableView
 from .index_view import IndexView
 from .auth_view import SmiapLoginView, SmiapLogoutView
 from .publications_view import PublicationView
--- main/views/timetable_view.py
+++ main/views/timetable_view.py
@@ -1,9 +1,10 @@
+import re
 from collections import defaultdict
 
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 
-from main.models import Group, Schedule, ExtramuralSchedule
+from main.models import Group, Schedule, ExtramuralSchedule, Teacher
 from utils.date import TeachTime, TeachState
 
 
@@ -85,4 +86,39 @@ class ExtramuralTimetableView(TemplateView):
             'items': items,
             'date_block': date_block(teach_time),
             'course': group.course if group_name else 0
+        })
+
+class TeacherTimetableView(TemplateView):
+    template_name = 'materials/timetable/teachers.html'
+
+    def get(self, request, *args, **kwargs):
+        teach_time = TeachTime()
+
+        teachers = Teacher.objects.filter(staff__isnull=False).order_by('lastname', 'firstname', 'middlename')
+        teacher_name = request.GET.get('teacher', str(teachers[0]))
+        lastname, firstname, middlename, _ = re.split('[ .]', teacher_name, maxsplit=3)
+        teacher = Teacher.objects.get(lastname=lastname,
+                                      firstname__startswith=firstname,
+                                      middlename__startswith=middlename)
+        week = request.GET.get('week',
+                               teach_time.week if teach_time.week <= teach_time.weeks_in_semester else teach_time.week)
+        items = Schedule.objects.prefetch_related('day', 'groups', 'teachers', 'places')\
+                                .filter(day__week=week, teachers__exact=teacher)\
+                                .order_by('day__date', 'starts_at')
+        schedule = defaultdict(list)
+        for item in items:
+            schedule[item.day].append(item)
+
+        if len(teachers) > 0:
+            weeks = teach_time.weeks_in_semester
+        else:
+            weeks = 0
+
+        return render(request, self.template_name, {
+            'teachers': teachers,
+            'teacher': teacher,
+            'weeks': weeks,
+            'week': week,
+            'schedule': schedule,
+            'date_block': date_block(teach_time)
         })
