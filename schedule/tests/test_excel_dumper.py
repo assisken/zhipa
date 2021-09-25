dac4f0eb55aacb5136bd64f0c91258e27bb2190a
Nikita Zhiga <assisken@gmail.com>
2021-09-26 02:53:51 +0300

    Fix tests

--- schedule/tests/test_excel_dumper.py
+++ schedule/tests/test_excel_dumper.py
@@ -2,7 +2,11 @@ import os
 
 from django.test import TestCase
 
-from schedule.management.scripts.generate import gen_groups_table, gen_teachers_table
+from schedule.management.scripts.generate import (
+    Config,
+    gen_groups_table,
+    gen_teachers_table,
+)
 from schedule.models import Group, Schedule, Teacher
 from smiap.settings import BASE_DIR
 
@@ -16,12 +20,29 @@ class TestExcelDumper(TestCase):
 
     def test_group_dump_creates(self):
         path = os.path.join(
-            BASE_DIR, "schedule", "tests", "files", "group_template.excel"
+            BASE_DIR, "schedule", "tests", "files", "group_template.xlsx"
         )
-        gen_groups_table(self.groups, from_week=1, wb_path=path)
+        config = Config(
+            template_path=path,
+            from_week=1,
+            to_week=2,
+            print_item_name=True,
+            print_item_type=True,
+            print_places=True,
+        )
+        gen_groups_table(self.groups, config)
 
     def test_teacher_dump_creates(self):
         path = os.path.join(
-            BASE_DIR, "schedule", "tests", "files", "teacher_template.excel"
+            BASE_DIR, "schedule", "tests", "files", "teacher_template.xlsx"
         )
-        gen_teachers_table(self.teachers, Schedule.STUDY, wb_path=path)
+        config = Config(
+            template_path=path,
+            from_week=1,
+            to_week=2,
+            print_item_name=True,
+            print_item_type=True,
+            print_places=True,
+            schedule_type=Schedule.STUDY,
+        )
+        gen_teachers_table(self.teachers, config)
